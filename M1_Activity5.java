5d5ee38744eaf7f5083f1f18c701010ac93ae6c7
Moses Luigi D. Ramos <mldramos1@headoffice.corp.bpi.com.ph>
2025-12-04 13:53:53 +0800

    Commit for M1_Activity5

---
+++ M1_Activity5.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class M1_Activity5 {
+	public static void main(String[] args) {
+	int sum=0;
+	for (int i = 0; i <= 50; i++)
+	{
+		sum +=i;
+	}
+	System.out.print("Sum = "+sum);
+	
+ }
+}
