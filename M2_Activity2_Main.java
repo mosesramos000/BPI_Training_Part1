bf81fabd391de38f296280070fff339cbba3bc0a
Moses Luigi D. Ramos <mldramos1@headoffice.corp.bpi.com.ph>
2025-12-04 19:32:57 +0800

    Commit for M2_Activity2

---
+++ M2_Activity2_Main.java
@@ -0,0 +1,11 @@
+package objectandclasses;
+
+public class M2_Activity2_Main {
+	public static void main(String[] args) {
+		M2_Activity2_Whiteboard wb = new M2_Activity2_Whiteboard();
+		int sum = wb.addNumbers(5,3);
+		
+		System.out.println("The total is: " + sum);
+	}
+
+}
---
+++ M2_Activity2_Whiteboard.java
@@ -0,0 +1,9 @@
+package objectandclasses;
+
+public class M2_Activity2_Whiteboard {
+	int addNumbers(int addend1, int addend2) {
+		int sum = addend1 + addend2;
+		return sum;
+	}
+
+}
